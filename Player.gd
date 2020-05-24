87d7b8bf9c6eea9c20287617406ad4e219b00946
Sam Corbitt <samcorbitt_@outlook.com>
2020-05-24 12:31:03 -0500

    Multiple
    - Added Tile Set
    - Added Slink's sprites
    - Made running animation
    - Made idle animation
    - (Partially) Made jump animation

--- Player.gd
+++ Player.gd
@@ -13,17 +13,24 @@ func _physics_process(delta):
 	# Moving left and right
 	if Input.is_action_pressed("dpad_right"):
 		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("dpad_left"):
 		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
 		motion.x = 0
+		$Sprite.play("Idle")
 
 	# Pressing "A" will make Slink jump.
 	if is_on_floor():
 		if Input.is_action_just_pressed("a"):
 			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("Jump")
 
-	if Input.is_action_just_pressed("select"):
+	if Input.is_action_just_pressed("start"):
 		get_tree().quit()
 
 	motion = move_and_slide(motion, UP)
