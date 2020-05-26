090614498384b891a6c95f4870076fb79e7c531b
Sam Corbitt <samcorbitt_@outlook.com>
2020-05-25 22:42:03 -0500

    Multiple
    - Fixed import deps error
    - Added Fall "animation"
    - Updated test world
    - Updated .gitignore to remove .vscode dir
    - Added acceleration to Slink

--- Player.gd
+++ Player.gd
@@ -1,34 +1,43 @@
 extends KinematicBody2D
 
+const ACCELERATION = 50
 const GRAVITY = 20
 const JUMP_HEIGHT = -550
-const SPEED = 350
+const MAX_SPEED = 350
 const UP = Vector2(0, -1)
 
 var motion = Vector2()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	motion.y += GRAVITY
+	var friction = false
 
 	# Moving left and right
 	if Input.is_action_pressed("dpad_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("dpad_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		friction = true
 		$Sprite.play("Idle")
 
 	# Pressing "A" will make Slink jump.
 	if is_on_floor():
 		if Input.is_action_just_pressed("a"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 
 	if Input.is_action_just_pressed("start"):
 		get_tree().quit()
